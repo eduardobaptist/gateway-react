5f57c916166991f962bd82d7bb380a345fae0954
eduardo baptista <aristimdudu@gmail.com>
2024-07-17 02:43:56 -0300

    organização do projeto e início da home/nav

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,34 @@
+import logo from '../../assets/img/gateway_logo_branco.png';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="navbar cabecalho d-flex justify-content-center">
+        <div className="container row">
+          <div className="col-6 d-flex justify-content-start">
+            <a className="" href="#">
+              <img
+                src={logo}
+                className="d-inline-block align-top logo"
+                alt="gateway__"
+              />
+            </a>
+          </div>
+          <div className="col-6 d-flex justify-content-end">
+            <button
+              type="button"
+              className="btn btn-light btn-logar"
+              data-bs-toggle="modal"
+              data-bs-target="#modal-login"
+            >
+              <span>Entrar</span>
+            </button>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
